3c1e591b56a3e62124f641de913820676eacef58
Igor <igormiekina@gmail.com>
2024-03-12 00:46:04 +0100

    Top Level Statemen

--- GLogic/Program.cs
+++ GLogic/Program.cs
@@ -1,96 +1,87 @@
-﻿using System.Diagnostics;
-using GLogic.Components.Common;
+﻿using GLogic.Components.Common;
 using GLogic.Jobs;
 using SDL2;
 
-namespace GLogic;
+// const int test = args[0] is not null ? Int32.Parse(args[0]) : 60;
 
-internal static class Program
+SDL.SDL_Init(SDL.SDL_INIT_EVERYTHING);
+SDL_ttf.TTF_Init();
+
+var appRenderer = new Renderer();
+var userActionHandler = new UserActionsHandler(appRenderer);
+var window = SDL.SDL_CreateWindow(
+    "GLogic",
+    SDL.SDL_WINDOWPOS_CENTERED,
+    SDL.SDL_WINDOWPOS_CENTERED,
+    appRenderer.WindowSize.Size.X,
+    appRenderer.WindowSize.Size.Y,
+    SDL.SDL_WindowFlags.SDL_WINDOW_SHOWN);
+var sdlRenderer = SDL.SDL_CreateRenderer(window, -1, SDL.SDL_RendererFlags.SDL_RENDERER_ACCELERATED);
+
+const int fps = 60;
+const int desiredDelta = 1000 / fps;
+
+var quit = false;
+while (!quit)
 {
-    public static void Main(string[] args)
+    var startLoop = SDL.SDL_GetTicks();
+
+    while (SDL.SDL_PollEvent(out var e) != 0)
     {
-        // const int test = args[0] is not null ? Int32.Parse(args[0]) : 60;
-
-        SDL.SDL_Init(SDL.SDL_INIT_EVERYTHING);
-        SDL_ttf.TTF_Init();
-
-        var appRenderer = new Renderer();
-        var userActionHandler = new UserActionsHandler(appRenderer);
-        var window = SDL.SDL_CreateWindow(
-            "GLogic",
-            SDL.SDL_WINDOWPOS_CENTERED,
-            SDL.SDL_WINDOWPOS_CENTERED,
-            appRenderer.WindowSize.Size.X,
-            appRenderer.WindowSize.Size.Y,
-            SDL.SDL_WindowFlags.SDL_WINDOW_SHOWN);
-        var sdlRenderer = SDL.SDL_CreateRenderer(window, -1, SDL.SDL_RendererFlags.SDL_RENDERER_ACCELERATED);
-
-        const int fps = 60;
-        const int desiredDelta = 1000 / fps;
-
-        var quit = false;
-        while (!quit)
+        if (e.type == SDL.SDL_EventType.SDL_MOUSEBUTTONUP)
         {
-            var startLoop = SDL.SDL_GetTicks();
-
-            while (SDL.SDL_PollEvent(out var e) != 0)
-            {
-                if (e.type == SDL.SDL_EventType.SDL_MOUSEBUTTONUP)
-                {
-                    userActionHandler.HandleMouseUpPollEvent(new Vector2Int(e.button.x, e.button.y), e.button.button);
-                }
-
-                if (e.type == SDL.SDL_EventType.SDL_MOUSEBUTTONDOWN)
-                {
-                    userActionHandler.HandleMouseDownPollEvent(new Vector2Int(e.button.x, e.button.y), e.button.button);
-                }
-
-                if (e.type == SDL.SDL_EventType.SDL_KEYDOWN)
-                {
-                    userActionHandler.HandleKeyDownEvent(e.key.keysym.sym);
-                }
-
-                if (e.type == SDL.SDL_EventType.SDL_KEYUP)
-                {
-                    userActionHandler.HandleKeyUpEvent(e.key.keysym.sym);
-                }
-
-                if (e.type == SDL.SDL_EventType.SDL_QUIT)
-                {
-                    quit = true;
-                }
-
-                if (e.type == SDL.SDL_EventType.SDL_MOUSEWHEEL)
-                {
-                    SDL.SDL_GetMouseState(out var cursorX, out var cursorY);
-                    var cursor = new Vector2Int(cursorX, cursorY);
-                    userActionHandler.HandleMouseWheel(cursor, e.wheel.y);
-                }
-            }
-
-            SDL.SDL_GetRelativeMouseState(out var relativeX, out var relativeY);
-            userActionHandler.HandleMouseHeldAction(new Vector2Int(relativeX, relativeY));
-
-            SDL.SDL_SetRenderDrawColor(sdlRenderer, 1, 1, 1, 255);
-            SDL.SDL_RenderClear(sdlRenderer);
-            appRenderer.RenderEntities(sdlRenderer);
-
-            Menu.Draw(sdlRenderer);
-
-            var delta = SDL.SDL_GetTicks() - startLoop;
-            if (delta < desiredDelta)
-            {
-                SDL.SDL_Delay(desiredDelta - delta);
-                Console.WriteLine($"{delta} ---- {desiredDelta}");
-            }
-            else
-            {
-                Console.WriteLine("Performance issue");
-            }
+            userActionHandler.HandleMouseUpPollEvent(new Vector2Int(e.button.x, e.button.y), e.button.button);
         }
 
-        SDL.SDL_DestroyRenderer(sdlRenderer);
-        SDL.SDL_DestroyWindow(window);
-        SDL.SDL_Quit();
+        if (e.type == SDL.SDL_EventType.SDL_MOUSEBUTTONDOWN)
+        {
+            userActionHandler.HandleMouseDownPollEvent(new Vector2Int(e.button.x, e.button.y), e.button.button);
+        }
+
+        if (e.type == SDL.SDL_EventType.SDL_KEYDOWN)
+        {
+            userActionHandler.HandleKeyDownEvent(e.key.keysym.sym);
+        }
+
+        if (e.type == SDL.SDL_EventType.SDL_KEYUP)
+        {
+            userActionHandler.HandleKeyUpEvent(e.key.keysym.sym);
+        }
+
+        if (e.type == SDL.SDL_EventType.SDL_QUIT)
+        {
+            quit = true;
+        }
+
+        if (e.type == SDL.SDL_EventType.SDL_MOUSEWHEEL)
+        {
+            SDL.SDL_GetMouseState(out var cursorX, out var cursorY);
+            var cursor = new Vector2Int(cursorX, cursorY);
+            userActionHandler.HandleMouseWheel(cursor, e.wheel.y);
+        }
+    }
+
+    SDL.SDL_GetRelativeMouseState(out var relativeX, out var relativeY);
+    userActionHandler.HandleMouseHeldAction(new Vector2Int(relativeX, relativeY));
+
+    SDL.SDL_SetRenderDrawColor(sdlRenderer, 1, 1, 1, 255);
+    SDL.SDL_RenderClear(sdlRenderer);
+    appRenderer.RenderEntities(sdlRenderer);
+
+    Menu.Draw(sdlRenderer);
+
+    var delta = SDL.SDL_GetTicks() - startLoop;
+    if (delta < desiredDelta)
+    {
+        SDL.SDL_Delay(desiredDelta - delta);
+        Console.WriteLine($"{delta} ---- {desiredDelta}");
+    }
+    else
+    {
+        Console.WriteLine("Performance issue");
     }
 }
+
+SDL.SDL_DestroyRenderer(sdlRenderer);
+SDL.SDL_DestroyWindow(window);
+SDL.SDL_Quit();
